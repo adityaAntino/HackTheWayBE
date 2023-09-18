33e9f020fec3f8352d772dc82791410247393b58
Aditya Arya <aarya@coligomed.com>
2023-09-18 23:43:15 +0530

    updated_4 .env

--- src/app/common/utils/twillio.js
+++ src/app/common/utils/twillio.js
@@ -8,10 +8,10 @@ const sendOTP = async (mobileNo) => {
 	try {
 		if (process.env.NODE_ENV != "production") return Response(true, "OTP sent successfully");
 
-		await client.verify.v2
-			.services(process.env.VERIFY_SERVICE_SID)
-			.verifications.create({ to: `+91${mobileNo}`, channel: "sms" });
-		return Response(true, "OTP sent successfully");
+		// await client.verify.v2
+		// 	.services(process.env.VERIFY_SERVICE_SID)
+		// 	.verifications.create({ to: `+91${mobileNo}`, channel: "sms" });
+		// return Response(true, "OTP sent successfully");
 	} catch (error) {
 		console.log(error);
 		return Response(false, "Error occurred during sending notification:", error);
@@ -22,15 +22,15 @@ const verifyOTP = async (mobileNo, otp) => {
 	try {
 		if (process.env.NODE_ENV != "production") return Response(true, "OTP verified successfully", { data: {} });
 
-		const response = await client.verify.v2
-			.services(process.env.VERIFY_SERVICE_SID)
-			.verificationChecks.create({ to: `+91${mobileNo}`, code: `${otp}` });
+		// const response = await client.verify.v2
+		// 	.services(process.env.VERIFY_SERVICE_SID)
+		// 	.verificationChecks.create({ to: `+91${mobileNo}`, code: `${otp}` });
 
-		if (response && response.status === "approved") {
-			return Response(true, "OTP verified successfully", { data: {} });
-		}
+		// if (response && response.status === "approved") {
+		// 	return Response(true, "OTP verified successfully", { data: {} });
+		// }
 
-		return Response(false, "Invalid otp");
+		// return Response(false, "Invalid otp");
 	} catch (error) {
 		return Response(false, "Error occurred during OTP verification", error);
 	}
